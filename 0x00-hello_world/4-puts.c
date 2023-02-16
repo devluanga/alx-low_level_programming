51f6f6bded622e863201bf9e9feb601f3b7ddd74
devluanga <imaniimaniluanga@gmail.com>
2023-02-16 05:38:21 -0800

    C program that uses puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
  int main (void)
 {
+	/**
+	 *main -Entry point of the code
+
+	 *Return (0)
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 
