0a95cc3b2e51dd56bdcbf1e21c47819cb496961b
devluanga <imaniimaniluanga@gmail.com>
2023-02-16 05:59:49 -0800

    C program using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,12 @@
 
 /**
  * main - entry of the program
- * return 0 always a a success
- * #include preprocessor
+ * Return: 0 always (success)
  */
 
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art\n");
-	return 0;
+	return (0);
 }
