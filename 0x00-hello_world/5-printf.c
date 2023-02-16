58fdbe08e4bbdb64e102b5126317830ca7e08920
devluanga <imaniimaniluanga@gmail.com>
2023-02-16 05:55:02 -0800

    C program using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry of the program
+ * return 0 always a a success
+ * #include preprocessor
+ */
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return 0;
+}
