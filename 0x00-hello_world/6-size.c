0eb9fa8f641307f34199df492201a69eb0e83aa1
devluanga <imaniimaniluanga@gmail.com>
2023-02-16 06:27:19 -0800

    check dtype sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
-*main - program entry
-*Return: always 0 (success)
+*main -Entry point
+*Return: Always 0 (Success)
 */
+
 
 int main(void)
 {
-	printf("Size of a char: %2d byte(s)\n", sizeof(char));
-	printf("Size of an int: %2d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %2d byte(s)\n", sizeof(long long int));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 	
