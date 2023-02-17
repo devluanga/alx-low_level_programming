5958949f79a84fcd8fafbe32da36b197939db374
devluanga <imaniimaniluanga@gmail.com>
2023-02-17 07:53:27 -0800

    alphatical letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,10 +6,12 @@
  */
 int main(void)
 {
-    char c;
-    for (c = 'a'; c <='z'; c++)
-    {
-        putchar(c);
-    }
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
 	return (0);
 }
