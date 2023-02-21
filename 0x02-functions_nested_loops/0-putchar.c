9840e94208ca2b5c8964d58e55cc8b85343f66eb
devluanga <imaniimaniluanga@gmail.com>
2023-02-21 05:40:50 -0800

    print _put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,13 +9,24 @@
 
 int main(void)
 {
-	int i;
+/*	int i;
 	char p[] = "_putchar";
 
 	for (i = 0; i < 9; i++)
 		{
 		_putchar(p[i]);
 		}
-		_putchar('\n');
+		_putchar('\n'); */
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
