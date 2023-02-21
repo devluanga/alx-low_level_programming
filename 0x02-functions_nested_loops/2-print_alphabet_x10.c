482bc8d05661c16ba7f5d115fdbc4441a0110122
devluanga <imaniimaniluanga@gmail.com>
2023-02-21 06:45:21 -0800

    alphabet letters 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- *void print_alphabet_x10(void) - Prints alphabets 10 times
- *
- *Return: always 0
- */
+*void print_alphabet_x10(void) - Prints alphabets 10 times
+*
+*Return: always 0
+*/
 
 void print_alphabet_x10(void)
 {
 	int x = 0;
 	char y;
 
-	while(x<10)
+	while (x < 10)
 	{
-		for (y = 'a'; y<='z'; y++)
+		for (y = 'a'; y <= 'z'; y++)
 		{
 			_putchar(y);
 		}
