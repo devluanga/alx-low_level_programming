ab57730498cb184b21ecafe90cafac4edf8e8662
devluanga <imaniimaniluanga@gmail.com>
2023-02-23 05:35:32 -0800

    check if the functions prints positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - test if the function prints positve or negative
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	positive_or_negative(x);
+
+	return (0);
+}
+
