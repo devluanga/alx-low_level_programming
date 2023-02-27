325b3abba6f7a930019513fec962fa212acfc59b
devluanga <imaniimaniluanga@gmail.com>
2023-02-27 09:28:39 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_rev - toma la cadena de la funcion.
+ * @s: value taken from main
+ * Return: (0)
+ */
+void print_rev(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+	}
+	for (count = count - 1; count >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar('\n');
+}
