3138ed5d16e87e477f2f106926e0abc5c8c886f1
devluanga <imaniimaniluanga@gmail.com>
2023-03-01 07:32:46 -0800

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 /**
  * _strcmp - compare string values
  * @s1: input value
@@ -8,15 +8,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int x;
+	int i;
 
-	for (x = 0; s1[x] != '\0' && s2[x] != '\0'; x++)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[x] != s2[x])
+		if (s1[i] != s2[i])
 		{
-			return (s1[x] - s2[x]);
+			return (s1[i] - s2[i]);
 		}
-		x++;
+		i++;
 	}
 	return (0);
 }
