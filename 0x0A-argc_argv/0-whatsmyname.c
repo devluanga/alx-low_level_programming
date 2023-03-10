b961fc58315f04eea9b4c8cecd0ec916f9baaae7
devluanga <imaniimaniluanga@gmail.com>
2023-03-10 10:32:02 -0800

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+int main(int argc, char argv[])
+{
+	char *program_name = strrchr(argv[0], '/');
+
+	if (program_name == NULL)
+	{
+		program_name = argv[0};
+	}
+	else
+	{
+		program_name++;
+	}
+	printf("Programe name: %s\n", program_name);
+	return (0);
+}
