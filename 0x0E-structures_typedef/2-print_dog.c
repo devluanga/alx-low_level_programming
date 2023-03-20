50a486c51ddd4a98d342ed836a41f2750c57a6ab
devluanga <imaniimaniluanga@gmail.com>
2023-03-20 13:20:58 +0000

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,10 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	printf("Name: %s\n", (d->name == NULL ? (nil) : d->name));
-	printf("Age: %f\n", (d->age == NULL ? (nil) : d->age));
-	printf("Owner: %s\n", (d->owner == NULL ? (nil) : d->owner));
+	else
+	{
+		printf("Name: %s\n", (d->name == NULL ? (nil) : d->name));
+		printf("Age: %f\n", (d->age == NULL ? (nil) : d->age));
+		printf("Owner: %s\n", (d->owner == NULL ? (nil) : d->owner));
+	}
 }
