ad59081594144da9b84c60fe910d8fbf64e25ce6
devluanga <imaniimaniluanga@gmail.com>
2023-03-20 11:55:53 +0000

    define struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_func
+#define dog_func
+/**
+ * struct dog - Define a new struct dog with name, age, owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
