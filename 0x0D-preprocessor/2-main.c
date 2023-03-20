21e03d53a5d9c5d6bd34b78dfb9bceed3662b229
devluanga <imaniimaniluanga@gmail.com>
2023-03-20 07:29:17 +0000

    print source code file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - start of the program
+ *Return: walys 0 to success
+ *
+ */
+int main(void)
+{
+	/**
+	 *we'll use printf because:
+	 *You are allowed to use the standard library
+	 */
+	printf("%s\n", __FILE__);
+	return (0);
+}
