7d127a4293687a8fbcd4bd626b217881d4f3e4c6
devluanga <imaniimaniluanga@gmail.com>
2023-03-22 03:24:30 -0700

    iterative

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - function executes a function on array
+ * @array: pointer to array
+ * @size: number elements in array
+ * @action: pointer to function
+ * Return: nothing
+ **/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	/**
+	 *the function checks if the input array "array" is NULL or empty,
+	 *or if the function pointer "action" is NULL.
+	 *If any of these conditions are true,
+	 *the function returns without iterating over the array.
+	 *Otherwise, the function pointer "action"
+	 *is dereferenced using the asterisk (*) operator,
+	 *and the function is called with each element
+	 *of the input array as an argument.
+	 *The for loop in this implementation uses the size_t data type
+	 *to keep track of the current index of the array.
+	 */
+	if (array == NULL || size == 0 || action == NULL)
+	{
+		return;
+	}
+	for (size_t i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
