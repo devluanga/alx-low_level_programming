4faa66da8e2b19fb4bc406cfc7e8fc9243764bac
devluanga <imaniimaniluanga@gmail.com>
2023-03-22 03:11:49 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,6 +9,15 @@
  **/
 void print_name(char *name, void (*f)(char *))
 {
+	/**
+	 *the function checks if the input string "name" is NULL or empty,
+	 *or if the function pointer "f" is NULL.
+	 *If any of these conditions are true,
+	 *the function returns without printing anything.
+	 *Otherwise, the function pointer "f"
+	 *is dereferenced using the asterisk (*) operator,
+	 *and the function is called with the input string "name" as an argument.
+	 */
 	if (name == NULL || *name == '\0' || f == NULL)
 	{
 		return;
