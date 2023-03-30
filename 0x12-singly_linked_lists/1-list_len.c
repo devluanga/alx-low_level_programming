b4e31fe2a9ecc1041f8ce1ff14b880ddcc4f2372
devluanga <imaniimaniluanga@gmail.com>
2023-03-30 21:58:55 +0000

    count elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * list_len - finds the number of nodes in a list
+ * @h: singly linked list
+ * Return: number of elements in a linked list
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
