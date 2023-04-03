9c567062efe23f8a76d43346ec965d0b40d91434
devluanga <imaniimaniluanga@gmail.com>
2023-04-03 17:55:13 +0000

    free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,9 +7,9 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp = malloc(sizeof(listint_t));
+	listint_t *temp = head;
 
-	while (head != NULL)
+	while (temp != NULL)
 	{
 		temp = temp->next;
 		free(head);
