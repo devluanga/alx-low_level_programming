89b68acbf70713270526d7e8c9990acd2e4cc2ac
devluanga <imaniimaniluanga@gmail.com>
2023-04-03 16:53:39 +0000

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,10 +14,10 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d \n", h->n);
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
-	printf("\n");
+	/*printf("\n");*/
 	return (count);
 }
